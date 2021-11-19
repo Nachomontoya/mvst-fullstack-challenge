0a7534d4eaac63f581f3e1190e0d21b805429683
Nachomontoya <montoyanacho@gmail.com>
2021-11-19 18:09:55 +0100

    added last dist folder

--- packages/server/dist/server.js
+++ packages/server/dist/server.js
@@ -12,8 +12,6 @@ var express_rate_limit_1 = __importDefault(require("express-rate-limit"));
 var routes_1 = require("./routes");
 exports.app = (0, express_1.default)();
 exports.app.use(express_1.default.json());
-exports.app.use(express_1.default.json({ limit: "50mb" }));
-exports.app.use(express_1.default.urlencoded({ limit: "50mb", extended: true }));
 exports.app.use((0, morgan_1.default)("dev"));
 exports.app.use((0, helmet_1.default)());
 exports.app.use((0, cors_1.default)());
