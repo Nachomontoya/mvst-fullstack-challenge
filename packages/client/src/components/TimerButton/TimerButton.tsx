8ba02dc98cdef75253e927ea901c2812d01537a8
Nachomontoya <montoyanacho@gmail.com>
2021-11-27 19:14:42 +0100

    refactor: cleaned functions on timerButton

--- packages/client/src/components/TimerButton/TimerButton.tsx
+++ packages/client/src/components/TimerButton/TimerButton.tsx
@@ -4,17 +4,26 @@ import play from "../../assets/play_btn.svg";
 import stop from "../../assets/stop_btn.svg";
 
 import { secondsToString } from "../../utils/formater";
-import { createNewTime, getTotalTime } from "../../api/timerApi";
+import { createNewTime } from "../../api/timerApi";
 import { CurrentTimer } from "../../utils/types";
 
 function TimerButton(): React.ReactElement {
   const [isLoading, setIsLoading] = useState(false);
   const [running, setRunning] = useState<boolean>(false);
-
   const [timerBtn, setTimerBtn] = useState<CurrentTimer>({
     time: 0,
     timeString: "00:00:00",
   });
+
+  const createNewLog = async (time: number) => {
+    setIsLoading(true);
+    try {
+      await createNewTime(time);
+    } catch (error: any) {
+      console.log(error.response.data);
+    }
+    setIsLoading(false);
+  };
 
   const startChrono = () => {
     setTimerBtn((prevState) => ({
@@ -23,58 +32,37 @@ function TimerButton(): React.ReactElement {
     }));
   };
 
-  console.log(timerBtn);
+  const stopChrono = () => {
+    setTimerBtn({
+      time: 0,
+      timeString: "00:00:00",
+    });
+    createNewLog(timerBtn.time);
+  };
 
-  // const stopChrono = () => {
-  //   setTimerBtn({
-  //     icon: play,
-  //     time: 0,
-  //     timeString: "00:00:00",
-  //   });
-  // };
-
-  // const createNewLog = async () => {
-  //   setIsLoading(true);
-  //   try {
-  //     await createNewTime(totalTimer.time);
-  //   } catch (error: any) {
-  //     console.log(error.response.data);
-  //   }
-  //   setIsLoading(false);
-  // };
+  const handleRunning = () => {
+    if (running) {
+      stopChrono();
+    }
+    setRunning(!running);
+  };
 
   useEffect(() => {
     const interval: NodeJS.Timeout = setInterval(startChrono, 1000);
     if (!running) {
       clearInterval(interval);
-      // stopChrono();
-      // setTotalTimer({ ...totalTimer, time: currentTimer.time });
     }
     return () => {
       clearInterval(interval);
     };
   }, [running]);
 
-  // useEffect(() => {
-  //   setTimerBtn({
-  //     ...timerBtn,
-  //     timeString: secondsToString(timerBtn.time),
-  //   });
-  // }, [currentTimer.time]);
-
-  // useEffect(() => {
-  //   if (totalTimer.time > 0) {
-  //     createNewLog();
-  //     loadTotalTime();
-  //   }
-  // }, [totalTimer.time]);
-
   return (
     <>
       <button
         type="button"
         className="btn btn-light w-152"
-        onClick={() => setRunning(!running)}
+        onClick={handleRunning}
         disabled={isLoading}
       >
         <img
