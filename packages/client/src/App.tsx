0c6e1bd51e13d762c2308cb83e980e796b360e30
Nachomontoya <montoyanacho@gmail.com>
2021-11-28 12:25:24 +0100

    feat: added ddynamic meta info with react-helmet-async

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import Home from "./pages/Home";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import { ToastContainer } from "react-toastify";
 
 import "./App.scss";
 
-function App() {
+function App(): React.ReactElement {
   return (
-    <>
+    <HelmetProvider>
+      <Helmet defaultTitle="MVST Challenge">
+        <meta
+          name="description"
+          content="MVST Timer Challenge by Nacho Montoya"
+        />
+      </Helmet>
       <Home />
       <ToastContainer draggable theme="colored" />
-    </>
+    </HelmetProvider>
   );
 }
 
